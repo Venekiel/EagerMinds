00450fb4f6e2dc9d9393277e010c104912efa08b
Jeronismo02 <jeronismo02@gmail.com>
2022-07-17 18:11:58 +0000

    Added ALS-Refactored plugin and inherited it

---
+++ Source/EagerMinds/Characters/EagerMindsCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AlsCharacterExample.h"
+#include "EagerMindsCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EAGERMINDS_API AEagerMindsCharacter : public AAlsCharacterExample
+{
+	GENERATED_BODY()
+	
+};
